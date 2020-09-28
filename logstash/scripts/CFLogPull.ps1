fb7d091c9e243d73325cc1afb55d932cf640d906
wwalker0307 <36755138+wwalker0307@users.noreply.github.com>
2020-09-28 11:44:33 -0500

    Update CFLogPull.ps1
    Added retention flag check, email notification when flag not set on enterprise zone, variables to specify starting and ending times to pull logs, and cleanup of existing calls.

--- logstash/scripts/CFLogPull.ps1
+++ logstash/scripts/CFLogPull.ps1
@@ -7,17 +7,18 @@ $SmtpServer = "Hostname/IP of SMTP server"
 $APIKey = "Global API Key"
 $CFEmail = "CloudFlare admin email address for API call"
 $OrgId = "Organization ID for audit log pulls"
+# The following variables specify the timeframe to pull logs. As per the API documentation, https://api.cloudflare.com/#logs-received-logs-received,
+# the ending time ($EM) must be at least 5 minutes prior to now. In addition, the total timeframe cannot exceed 1 hour
+$SM = "Starting time number of minutes to go back - If it's 12:00 and you want logs starting at 11:50, set to 10"
+$EM = "Ending time number of minutes to go back - This must be at least 5 minutes in the past."
 
 #CloudFlare Enterprise Log Service (ELS) Pull
-#Get time 7m - 2m ago (In UTC)
 $D = Get-Date
 $T = (Get-Date ($D) -Format MMddyyy_HHmmss)
-$S = [Math]::Floor([decimal](Get-Date($D).AddMinutes(-7).ToUniversalTime()-uformat "%s"))
-$E = [Math]::Floor([decimal](Get-Date($D).AddMinutes(-2).ToUniversalTime()-uformat "%s"))
+$S = [Math]::Floor([decimal](Get-Date($D).AddMinutes(-$($SM)).ToUniversalTime()-uformat "%s"))
+$E = [Math]::Floor([decimal](Get-Date($D).AddMinutes(-$($EM)).ToUniversalTime()-uformat "%s"))
 #Force PowerShell to use TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12
-#Set Required Headers
-#Change GLOBALAPIKEY and USEREMAILADDRESS, retain double quotes.
 $A = @{
   ContentType = 'application/json'
   Method = 'GET'
@@ -26,44 +27,63 @@ $A = @{
       "X-Auth-Email" = $CFEmail
   }
 }
+$apibase = "https://api.cloudflare.com/client/v4"
+
 #Pull logs from all ELS enabled zones.
 #This will loop through all currently registered domains and, if they have an Enterprise Website plan, pull the logs.
-$Zones = Invoke-RestMethod @A "https://api.cloudflare.com/client/v4/zones?per_page=100" | Select -ExpandProperty Result
+$RetFlagNot = @()
+$Zones = (Invoke-RestMethod @A "$($apibase)/zones?per_page=100").Result
 foreach ($zone in $zones) {
   if ($zone.plan.name -eq "Enterprise Website") {
     $Z = $zone.id
-    $GetFields = Invoke-RestMethod @a -uri "https://api.cloudflare.com/client/v4/zones/$z/logs/received/fields"
-    $Fields = $GetFields.psobject.properties.name -join ","
-    $Known = "CacheCacheStatus,CacheResponseBytes,CacheResponseStatus,CacheTieredFill,ClientASN,ClientCountry,ClientDeviceType,ClientIP,ClientIPClass,ClientRequestBytes,ClientRequestHost,ClientRequestMethod,ClientRequestPath,ClientRequestProtocol,ClientRequestReferer,ClientRequestURI,ClientRequestUserAgent,ClientSSLCipher,ClientSSLProtocol,ClientSrcPort,ClientXRequestedWith,EdgeColoCode,EdgeColoID,EdgeEndTimestamp,EdgePathingOp,EdgePathingSrc,EdgePathingStatus,EdgeRateLimitAction,EdgeRateLimitID,EdgeRequestHost,EdgeResponseBytes,EdgeResponseCompressionRatio,EdgeResponseContentType,EdgeResponseStatus,EdgeServerIP,EdgeStartTimestamp,FirewallMatchesActions,FirewallMatchesRuleIDs,FirewallMatchesSources,OriginIP,OriginResponseBytes,OriginResponseHTTPExpires,OriginResponseHTTPLastModified,OriginResponseStatus,OriginResponseTime,OriginSSLProtocol,ParentRayID,RayID,SecurityLevel,WAFAction,WAFFlags,WAFMatchedVar,WAFProfile,WAFRuleID,WAFRuleMessage,WorkerCPUTime,WorkerStatus,WorkerSubrequest,WorkerSubrequestCount,ZoneID"
-    if ($Fields.length -ne $Known.length) {
-      #Create list of changes
-      $Change = Compare-Object ($known.Split(',')) ($fields.Split(',')) | Select -ExpandProperty InputObject | foreach {Write-Output "$_"`n}
-      #Determine whether or not to send notification based on existence of file in temp directory
-      $Notify = Test-Path C:\temp\FieldChange_$(Get-Date ($D) -Format MMddyyy_HH00).txt
-      #If file does not exist for the current hour, send notification
-      if ($Notify -eq $false) {
-      $Change | out-file C:\temp\FieldChange_$(Get-Date ($D) -Format MMddyyy_HH00).txt
-        if ($Fields.length -lt $Known.length) {
-          $Body = "The CloudFlare log pull script on SSSELKLOG has detected a change in field availability:`n`nFields removed:`n" + $Change
-          Send-MailMessage -Body $Body -Encoding UTF8 -From $SmtpFrom -SmtpServer $SmtpServer -Subject "Change To CloudFlare Fields" -To $SmtpTo
-        } else {
-          $Body = "The CloudFlare log pull script on SSSELKLOG has detected a change in field availability:`n`nFields added:`n" + $Change
-          Send-MailMessage -Body $Body -Encoding UTF8 -From $SmtpFrom -SmtpServer $SmtpServer -Subject "Change To CloudFlare Fields" -To $SmtpTo
+    if (((Invoke-RestMethod @a "$($apibase)/zones/$z/logs/control/retention/flag").result.flag) -ne $true) {
+      $RetFlagNot += @{$zone.name=$zone.id}
+      } else {
+        $Fields = (Invoke-RestMethod @a -uri "$($apibase)/zones/$z/logs/received/fields").psobject.properties.name -join ","
+        $Known = "CacheCacheStatus,CacheResponseBytes,CacheResponseStatus,CacheTieredFill,ClientASN,ClientCountry,ClientDeviceType,ClientIP,ClientIPClass,ClientRequestBytes,ClientRequestHost,ClientRequestMethod,ClientRequestPath,ClientRequestProtocol,ClientRequestReferer,ClientRequestURI,ClientRequestUserAgent,ClientSSLCipher,ClientSSLProtocol,ClientSrcPort,ClientXRequestedWith,EdgeColoCode,EdgeColoID,EdgeEndTimestamp,EdgePathingOp,EdgePathingSrc,EdgePathingStatus,EdgeRateLimitAction,EdgeRateLimitID,EdgeRequestHost,EdgeResponseBytes,EdgeResponseCompressionRatio,EdgeResponseContentType,EdgeResponseStatus,EdgeServerIP,EdgeStartTimestamp,FirewallMatchesActions,FirewallMatchesRuleIDs,FirewallMatchesSources,OriginIP,OriginResponseBytes,OriginResponseHTTPExpires,OriginResponseHTTPLastModified,OriginResponseStatus,OriginResponseTime,OriginSSLProtocol,ParentRayID,RayID,SecurityLevel,WAFAction,WAFFlags,WAFMatchedVar,WAFProfile,WAFRuleID,WAFRuleMessage,WorkerCPUTime,WorkerStatus,WorkerSubrequest,WorkerSubrequestCount,ZoneID"
+        if ($Fields.length -ne $Known.length) {
+          #Create list of changes
+          $Change = Compare-Object ($known.Split(',')) ($fields.Split(',')) | Select -ExpandProperty InputObject | foreach {Write-Output "$_"`n}
+          #Determine whether or not to send notification based on existence of file in temp directory
+          $Notify = Test-Path C:\temp\FieldChange_$(Get-Date ($D) -Format MMddyyy_HH00).txt
+          #If file does not exist for the current hour, send notification
+          if ($Notify -eq $false) {
+          $Change | out-file C:\temp\FieldChange_$(Get-Date ($D) -Format MMddyyy_HH00).txt
+            if ($Fields.length -lt $Known.length) {
+              $Body = "The CloudFlare log pull script has detected a change in field availability:`n`nFields removed:`n" + $Change
+              Send-MailMessage -Body $Body -Encoding UTF8 -From $SmtpFrom -SmtpServer $SmtpServer -Subject "Change To CloudFlare Fields" -To $SmtpTo
+            } else {
+              $Body = "The CloudFlare log pull script has detected a change in field availability:`n`nFields added:`n" + $Change
+              Send-MailMessage -Body $Body -Encoding UTF8 -From $SmtpFrom -SmtpServer $SmtpServer -Subject "Change To CloudFlare Fields" -To $SmtpTo
+            }
+          }
         }
+        $R = Invoke-RestMethod @A "$($apibase)/zones/$z/logs/received?start=$S&end=$E&timestamps=unix&fields=$Fields"
+        #Performs a character check of the log request. If the resulting output is empty, no file will be saved.
+        if ($R.length -gt 0) {
+        $R | Out-File "$path\CloudFlare$T.txt" -Encoding utf8 -Append
       }
-    }
-    $R = Invoke-RestMethod @A "https://api.cloudflare.com/client/v4/zones/$z/logs/received?start=$S&end=$E&timestamps=unix&fields=$Fields"
-    #Performs a character check of the log request. If the resulting output is empty, no file will be saved.
-    $C = $R | Measure -Character | Select -ExpandProperty Characters
-    if ($C -gt 0) {
-    $R | Out-File "$path\CloudFlare$T.txt" -Encoding utf8 -Append
     }
   }
 }
+#Send notification of zones without Retention Flag enabled.
+if ($RetFlagNot -gt 0) {
+  $RetFlagBody = "The Enterprise licensed zones listed below do not have logging enabled.
+$($RetFlagNot|ft -auto -hidetableheaders|Out-String)
+To enable logging, contact your account representative or run the following API call.
+Important: Enter your API Key, Email Address, and zone ID where necessary before running.`n
+PowerShell
+`$A = @{`n  ContentType = 'application/json'`n  Method = 'GET'`n  Headers = @{`n      'X-Auth-Key' = 'APIKEY'`n      'X-Auth-Email' = 'AuthorizedUserEmail'`n  }`n}
+Invoke-RestMethod @A -Uri 'https://api.cloudflare.com/client/v4/zones/ZONEID/logs/control/retention/flag?flag=true'`n`n
+Linux
+curl -X POST `"https://api.cloudflare.com/client/v4/zones/ZONEID/logs/control/retention/flag`" \`n     -H `"X-Auth-Email: AuthorizedUserEMail`" \`n     -H `"X-Auth-Key: APIKEY`" \`n     -H `"Content-Type: application/json`" \`n     --data '{`"flag`":true}'"
+  Send-MailMessage -Body $RetFlagBody -Encoding UTF8 -From $SmtpFrom -SmtpServer $SmtpServer -Subject "CloudFlare Logging Disabled" -To $SmtpTo
+}
+
 #Pull Organization audit logs.
 $Since = (Get-Date ($D).AddMinutes(-6).AddSeconds(-($D).second) -Format yyyy-MM-ddThh:mm:ssZ)
-$L = Invoke-RestMethod @A -Uri "https://api.cloudflare.com/client/v4/organizations/$OrgId/audit_logs?per_page=1000&since=$Since" | Select -ExpandProperty Result
-if (($L | Measure | Select -ExpandProperty Count) -gt 0) {
+$L = Invoke-RestMethod @A -Uri "$($apibase)/organizations/$OrgId/audit_logs?per_page=1000&since=$Since" | Select -ExpandProperty Result
+if (($L.result.length) -gt 0) {
   foreach ($auditrecord in $L) {
     [array]$records += $auditrecord | ConvertTo-Json -Compress
   }
